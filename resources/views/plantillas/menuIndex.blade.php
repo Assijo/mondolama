edd075ee7ecc767c49eabd8acc45799d8ac9add1
Assijo <SaisojE@outlook.com>
2021-01-31 17:49:56 -0700

    s

--- resources/views/plantillas/menuIndex.blade.php
+++ resources/views/plantillas/menuIndex.blade.php
@@ -3,7 +3,7 @@
 <nav id="custom-navbar" class="navbar navbar-expand-sm custom-navbar" height="100px">
     <nav class="navbar">
       <a class="navbar-brand" href="{{url('index')}}">
-          <img src="{{ asset('images/hoja.svg') }}" width="60" height="60" alt="" loading="lazy" class="img-logo-index">
+          <img src="{{ asset('images/mondolama.jpg') }}" width="60" height="60" alt="" loading="lazy" class="img-logo-index">
       </a>
     </nav>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
